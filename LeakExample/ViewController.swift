0491afe4aad02c6e17aaeced7feb3c9a1cb0d4f7
miyasaka_kazutoshi <miyasaka_kazutoshi@yahoo.co.jp>
2019-07-22 13:17:50 +0900

    Initial Commit

---
+++ LeakExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeakExample
+//
+//  Created by kazutoshi miyasaka on 2019/07/22.
+//  Copyright © 2019 jp.po-miyasaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
